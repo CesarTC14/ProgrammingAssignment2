1cfe9888659706549798841725a5f1651373f88f
CesarTC14 <41241616+CesarTC14@users.noreply.github.com>
2018-08-04 12:20:04 -0300

    completed R script

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
+## SET/GET/CACHE MATRIX INVERSE ##
 
-## Write a short comment describing this function
+## This function first tests if a matrix x is invertible, then creates a list of functions to allow caching the matrix inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        if(det(x) == 0) {
+        stop(paste("matrix", "is", "not", "invertible", sep = " "))
+        }
+        m <- NULL
+        set <- function(y){
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() {
+                x
+        }
+        setinverse <- function(inv) {
+                m <<- inv
+        }
+        getinverse <- function() {
+                m
+        }
+        list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## This function gets the inverse of matrix x or simply return it if already calculated
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        m <- x$getinverse()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        else {
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setinverse(m)
+        m
+        }
 }
